245061c6615b219ce9009ae54b1beb7aa16c6b2c
Ragan Hagey <raganahagey@gmail.com>
2022-09-23 12:13:02 -0400

    commit tree 38c0a6aa5cc75a7a6448199090a71374a48bb4a5

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+   return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    return bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "your mom"
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "my mom"
+}
